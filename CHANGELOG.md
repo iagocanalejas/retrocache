1aaa580c029136c41a8929e92ba7233c78d96c1c
IagoCanalejas <iagocanalejas@gmail.com>
2017-01-24 09:48:55 +0100

    Creating new tests + fixing the old ones + added clone method to interface to avoid casting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+# 1.1.1
+    - Added `clone()` method to `CachedCall` interface to avoid extra casting.
+    - Implemented new tests and fixed old ones.
+
 # 1.1
     - Added `remove` method for invalidate a cached call.
--- retrocache/src/test/java/com/andiag/retrocache/CallbackTest.java
+++ retrocache/src/test/java/com/andiag/retrocache/CallbackTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -27,7 +26,7 @@ import retrofit2.http.GET;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 /**
@@ -57,30 +56,7 @@ public class CallbackTest {
     }
 
     @Test
-    public void simpleCall() throws InterruptedException {
-        /* Set up the mock webserver */
-        MockResponse resp = new MockResponse().setBody("VERY_BASIC_BODY");
-        mServer.enqueue(resp);
-
-        DemoService demoService = mRetrofit.create(DemoService.class);
-
-        demoService.getHome().enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                assertEquals(response.body(), "VERY_BASIC_BODY");
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                t.printStackTrace();
-                fail("Failure executing the request");
-            }
-        });
-
-    }
-
-    @Test
-    public void cachedCall() throws Exception {
+    public void simpleCachedCall() {
         /* Set up the mock webserver */
         MockResponse resp = new MockResponse().setBody("VERY_BASIC_BODY");
         mServer.enqueue(resp);
@@ -92,6 +68,7 @@ public class CallbackTest {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
                 assertEquals(response.body(), "VERY_BASIC_BODY");
+                assertNotNull(mMockCachingSystem.get(Utils.urlToKey(call.request().url())));
             }
 
             @Override
@@ -99,8 +76,6 @@ public class CallbackTest {
                 fail("Failure executing the request: " + t.getMessage());
             }
         });
-
-        assertNotNull(mMockCachingSystem.get(Utils.urlToKey(call.request().url())));
     }
 
     @Test
@@ -109,19 +84,15 @@ public class CallbackTest {
         MockResponse resp = new MockResponse().setBody("VERY_BASIC_BODY");
         mServer.enqueue(resp);
 
-        Retrofit r = new Retrofit.Builder()
-                .baseUrl(mServer.url("/"))
-                .addConverterFactory(new ToStringConverterFactory())
-                .addCallAdapterFactory(buildSmartCacheFactory())
-                .build();
-        DemoService demoService = r.create(DemoService.class);
+        DemoService demoService = mRetrofit.create(DemoService.class);
+        CachedCall<String> call = demoService.getHome();
 
-        final CountDownLatch latch = new CountDownLatch(1);
-        final AtomicReference<Response<String>> responseRef = new AtomicReference<>();
-        demoService.getHome().enqueue(new Callback<String>() {
+        final CountDownLatch latch = new CountDownLatch(1); // Make main thread wait call to end.
+        call.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                responseRef.set(response);
+                assertEquals(response.body(), "VERY_BASIC_BODY");
+                assertNotNull(mMockCachingSystem.get(Utils.urlToKey(call.request().url())));
                 latch.countDown();
             }
 
@@ -130,48 +101,9 @@ public class CallbackTest {
                 fail("Failure executing the request: " + t.getMessage());
             }
         });
-        assertTrue(latch.await(1, TimeUnit.SECONDS));
-        assertEquals(responseRef.get().body(), "VERY_BASIC_BODY");
-
-        final CountDownLatch latch2 = new CountDownLatch(1);
-        final AtomicReference<Response<String>> response2Ref = new AtomicReference<>();
-        demoService.getHome().enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                latch2.countDown();
-                if (latch2.getCount() == 0) { // the cache hit one.
-                    response2Ref.set(response);
-                } else { // the network one.
-                    assertEquals(response.body(), response2Ref.get().body());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                fail("Failure executing the request: " + t.getMessage());
-            }
-        });
-        assertTrue(latch2.await(1, TimeUnit.SECONDS));
-
-        // Remove cached call
-        demoService.getHome().remove();
-        final CountDownLatch latch3 = new CountDownLatch(1);
-        final AtomicReference<Response<String>> response3Ref = new AtomicReference<>();
-        demoService.getHome().enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                response3Ref.set(response);
-                latch3.countDown();
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                fail("Failure executing the request: " + t.getMessage());
-            }
-        });
-        assertTrue(latch3.await(1, TimeUnit.SECONDS));
-        assertEquals(response3Ref.get().body(), "VERY_BASIC_BODY");
-        mServer.shutdown();
+        latch.await(1, TimeUnit.SECONDS);
+        call.remove();
+        assertNull(mMockCachingSystem.get(Utils.urlToKey(call.request().url())));
     }
 
     @Test
@@ -180,19 +112,16 @@ public class CallbackTest {
         MockResponse resp = new MockResponse().setBody("VERY_BASIC_BODY");
         mServer.enqueue(resp);
 
-        Retrofit r = new Retrofit.Builder()
-                .baseUrl(mServer.url("/"))
-                .addConverterFactory(new ToStringConverterFactory())
-                .addCallAdapterFactory(buildSmartCacheFactory())
-                .build();
-        DemoService demoService = r.create(DemoService.class);
+        DemoService demoService = mRetrofit.create(DemoService.class);
+        CachedCall<String> call = demoService.getHome();
+        CachedCall<String> call2Refresh = call.clone();
 
-        final CountDownLatch latch = new CountDownLatch(1);
-        final AtomicReference<Response<String>> responseRef = new AtomicReference<>();
-        demoService.getHome().enqueue(new Callback<String>() {
+        final CountDownLatch latch = new CountDownLatch(1); // Make main thread wait call to end.
+        call.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                responseRef.set(response);
+                assertEquals(response.body(), "VERY_BASIC_BODY");
+                assertNotNull(mMockCachingSystem.get(Utils.urlToKey(call.request().url())));
                 latch.countDown();
             }
 
@@ -201,20 +130,16 @@ public class CallbackTest {
                 fail("Failure executing the request: " + t.getMessage());
             }
         });
-        assertTrue(latch.await(1, TimeUnit.SECONDS));
-        assertEquals(responseRef.get().body(), "VERY_BASIC_BODY");
+        latch.await(1, TimeUnit.SECONDS);
 
-        final CountDownLatch latch2 = new CountDownLatch(1);
-        final AtomicReference<Response<String>> response2Ref = new AtomicReference<>();
-        demoService.getHome().enqueue(new Callback<String>() {
+        resp = new MockResponse().setBody("VERY_BASIC_REFRESHED_BODY");
+        mServer.enqueue(resp);
+
+        call2Refresh.refresh(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                latch2.countDown();
-                if (latch2.getCount() == 0) { // the cache hit one.
-                    response2Ref.set(response);
-                } else { // the network one.
-                    assertEquals(response.body(), response2Ref.get().body());
-                }
+                assertEquals(response.body(), "VERY_BASIC_REFRESHED_BODY");
+                assertNotNull(mMockCachingSystem.get(Utils.urlToKey(call.request().url())));
             }
 
             @Override
@@ -222,26 +147,6 @@ public class CallbackTest {
                 fail("Failure executing the request: " + t.getMessage());
             }
         });
-        assertTrue(latch2.await(1, TimeUnit.SECONDS));
-
-        // Remove cached call
-        final CountDownLatch latch3 = new CountDownLatch(1);
-        final AtomicReference<Response<String>> response3Ref = new AtomicReference<>();
-        demoService.getHome().refresh(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                response3Ref.set(response);
-                latch3.countDown();
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                fail("Failure executing the request: " + t.getMessage());
-            }
-        });
-        assertTrue(latch3.await(1, TimeUnit.SECONDS));
-        assertEquals(response3Ref.get().body(), "VERY_BASIC_BODY");
-        mServer.shutdown();
     }
 
     private static class MainThreadExecutor implements Executor {
